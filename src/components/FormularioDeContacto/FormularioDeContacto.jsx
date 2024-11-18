a41e12e461b854e637f167594d5f8cffbbb3baba
Sebastian Gomez <sebasgomez99@gmail.com>
2024-11-18 10:06:53 -0500

    lista formulario

--- src/components/FormularioDeContacto/FormularioDeContacto.jsx
+++ src/components/FormularioDeContacto/FormularioDeContacto.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react"
-
+import "./FormularioDeContacto.css"
 const FormularioDeContacto = () => {
     const [name, setName] = useState ("")
     const [email, setEmail] = useState ("")
     const [address, setAddress] = useState ("")
     const [phone, setPhone] = useState ("")
     const [historial, setHistorial]= useState ([])
+
 
     const handleNameChanges = (event) => {
       setName(event.target.value)
@@ -23,24 +24,27 @@ const FormularioDeContacto = () => {
       event.preventDefault()
       const user = {name, email, address, phone}
       setHistorial([...historial, user])
-      console.log([...historial, user]);
+      setName("")
+      setEmail("")
+      setAddress("")
+      setPhone("")
         }
   return (
     <>
-    <form>
+    <form className="contenedor-form" id="form">
       <label>Nombre</label>
-      <input type="text" onChange= {handleNameChanges} value={name}/>
+      <input type="text" onChange= {handleNameChanges} value={name} placeholder="Nombre"/>
       <label>Email</label>
-      <input type="email" onChange= {handleEmailChanges} value={email}/>
+      <input type="email" onChange= {handleEmailChanges} value={email} placeholder="Email"/>
       <label>Dirección</label>
-      <input type="text" onChange={handleAddressChange} value={address} />
+      <input type="text" onChange={handleAddressChange} value={address} placeholder="Dirección"/>
       <label>Número de Telefono</label>
-      <input type="number" onChange={handleNumberChange} value={phone}/>
-      <button onClick={handleHistorial} type="submit">Submit</button>
+      <input type="number" onChange={handleNumberChange} value={phone} placeholder="Telefono"/>
+      <button onClick={handleHistorial} type="submit" className="submit-button">Submit</button>
     </form>
     <div>
-      <h3>Historial</h3>
-      <ol>
+      <h3 className="titulo-historial">Historial</h3>
+      <ol className="historial-lista">
         {
           historial.map((historia, index) => (
             <li key={index}>
